daf55c2124c7bf6a6eba7874a4ba8d9c18b0e9b8
Amrithamani <amrimani91@gmail.com>
2017-06-12 16:01:14 -0400

    Committing integer to String variable project.

---
+++ src/session7/IntToString.java
@@ -0,0 +1,25 @@
+package session7;
+
+public class IntToString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// initialize integer variable
+
+		int integer = 5345;
+
+		// initialize string variable
+
+		String string;
+
+		// converting integer to string using valueOf function
+
+		string = String.valueOf(integer);
+
+		System.out.println(string);
+		// prints 5345
+
+	}
+
+}
